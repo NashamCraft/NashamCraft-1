7e1f33c53103c660434351701bcfacbd80dc3e74
nasham23 <nashamcraft@gmail.com>
2025-06-25 23:35:21 +0300

    new bosses tag to be blacklisted from apothic spawners

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -7,4 +7,31 @@ ServerEvents.tags('item', event => {
         'cataclysm:bulwark_of_the_flame'
     ])
 
+    event.add('c:bosses', [
+        "cataclysm:amethyst_crab",
+        "cataclysm:ancient_remnant",
+        "cataclysm:coralssus",
+        "cataclysm:coral_golem",
+        "cataclysm:ender_golem",
+        "cataclysm:ender_guardian",
+        "cataclysm:ignis",
+        "cataclysm:ignited_revenant",
+        "cataclysm:ignited_berserker",
+        "cataclysm:kobolediator",
+        "cataclysm:maledictus",
+        "cataclysm:aptrgangr",
+        "cataclysm:modern_remnant",
+        "cataclysm:netherite_monstrosity",
+        "cataclysm:netherite_ministrosity",
+        "cataclysm:the_baby_leviathan",
+        "cataclysm:the_harbinger",
+        "cataclysm:the_leviathan",
+        "cataclysm:the_prowler",
+        "cataclysm:wadjet",
+        "cataclysm:scylla",
+        "cataclysm:clawdian",
+        "irons_spellbooks:dead_king",
+        "irons_spellbooks:fire_boss"
+    ])
+
 })
